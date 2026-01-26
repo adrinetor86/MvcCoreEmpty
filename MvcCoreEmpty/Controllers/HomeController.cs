b9a9509a5beaaac6c9d6665f1a6472abc644a6b1
adrinetor86 <adrinetor81@gmail.com>
2026-01-26 14:12:26 +0100

    14:12

--- MvcCoreEmpty/Controllers/HomeController.cs
+++ MvcCoreEmpty/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreEmpty.Models;
 
 namespace MvcCoreEmpty.Controllers
 {
@@ -7,6 +8,24 @@ namespace MvcCoreEmpty.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Vista1()
+        {
+            return View();
+        }
+        public IActionResult Vista2()
+        {
+            return View();
+        }
+
+        public IActionResult VistaPersona()
+        {
+            Persona persona = new Persona();
+            persona.Nombre = "Alumno";
+            persona.Email = "alumno@tajamar365.com";
+            persona.Edad = 67;
+            return View(persona);
         }
     }
 }
--- MvcCoreEmpty/Program.cs
+++ MvcCoreEmpty/Program.cs
@@ -6,6 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
+//wwwroot
+app.UseStaticFiles();
+//MVC
 //app.MapGet("/", () => "Hello World!");
 app.MapControllerRoute(
     name: "default",
